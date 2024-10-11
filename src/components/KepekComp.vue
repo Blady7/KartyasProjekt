6eff85c262b270e5fa5fb7a6356ca455d6c23290
Blady7 <petihuawei2@gmail.com>
2024-10-11 10:35:29 +0200

    character button

--- src/components/KepekComp.vue
+++ src/components/KepekComp.vue
@@ -6,7 +6,7 @@
           <slot name="name"></slot>
           <button
             type="button"
-            class="btn btn-primary"
+            class="search-button"
             data-bs-toggle="modal"
             data-bs-target="#charInfoModal"
             @click="onClickReszletek()"
@@ -30,4 +30,16 @@
   </script>
   
   <style>
+.search-button {
+  background-color: #e79510; /* Narancssárga gomb */
+  border: none; /* Nincs keret a gomb körül */
+  font-weight: bold; /* Vastag betűtípus */
+  color: white; /* Fehér szín a gomb szövegéhez */
+  border-radius: 12px; /* Lekerekített sarkok */
+  padding: 5px 10px; /* Extra padding a gomb körül */
+}
+
+.search-button:hover {
+  background-color: #e67e22; /* Sötétebb narancssárga hover állapotban */
+}
   </style>
