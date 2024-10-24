1decdcbea3de58c655f17b314f3c45bd985fe71b
Blady7 <petihuawei2@gmail.com>
2024-10-24 10:24:55 +0200

    homeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="image-container">
+      <img src="/public/Genshin.png" alt="Genshin Impact" />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  }
+  </script>
+  
+<style>
+  .image-container {
+    text-align: center;
+    margin: 0;
+    padding: 0;
+    margin-top: -90px;
+  }
+  
+  .image-container img {
+    width: 700px; 
+    height: auto;
+    display: block; 
+    margin: 0 auto; 
+  }
+</style>
+  
